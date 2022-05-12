97efd57fa23a2fa005df116d2af7b970175f0419
BlackFox <fiona.speranskih@yandex.ru>
2022-05-12 20:31:45 +0500

    Добавьте файлы проекта.

---
+++ Client/Program.cs
@@ -0,0 +1,46 @@
+﻿using System.Text;
+using System.IO.Pipes;
+using System.Security.Principal;
+
+public class Client
+{
+    static string pipeName = "testpipe";
+
+    static byte[] data;
+    static NamedPipeClientStream pipeClient;
+
+    public static void Main(string[] args)
+    {
+        Initialise();
+        Send();
+    }
+
+    static void Initialise()
+    {
+        data = new byte[1024];
+        pipeClient = new NamedPipeClientStream(".", pipeName, PipeDirection.InOut, PipeOptions.None, TokenImpersonationLevel.Impersonation);
+    }
+
+    static void Send()
+    {
+        pipeClient.Connect();
+
+        Console.Write("Введите имя файла, который хотите сохранить на сервере: ");
+        string filename = Console.ReadLine() ?? "";
+
+        data[0] = (byte)filename.Length;
+        Encoding.UTF8.GetBytes(filename, 0, filename.Length, data, 1);
+
+        pipeClient.Write(data, 0, data.Length);
+
+        using (var fstream = new FileStream(filename, FileMode.Open))
+        {
+            while (fstream.Read(data, 0, data.Length) > 0)
+            {
+                pipeClient.Write(data, 0, data.Length);
+            }
+        }
+
+        pipeClient.Close();
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,57 @@
+﻿using System.Text;
+using System.IO.Pipes;
+
+class Server
+{
+    static string pipeName = "testpipe";
+
+    static byte[] data;
+    static StringBuilder filename;
+    static NamedPipeServerStream pipeServer;
+
+    static void Main()
+    {
+        Initialise();
+        Listening();
+    }
+
+    static void Initialise()
+    {
+        data = new byte[1024];
+        filename = new StringBuilder();
+        pipeServer = new NamedPipeServerStream(pipeName, PipeDirection.InOut);
+    }
+
+    static void Listening()
+    {
+        int size;
+        pipeServer.WaitForConnection();
+
+        try
+        {
+            pipeServer.Read(data, 0, data.Length);
+            filename.Append(Encoding.UTF8.GetString(data, 1, data[0]));
+
+            Console.WriteLine(filename);
+
+            using (FileStream fstream = new FileStream(filename.ToString(), FileMode.OpenOrCreate))
+            {
+                do
+                {
+                    size = pipeServer.Read(data);
+                    fstream.Write(data, 0, size);
+
+                } while (pipeServer.IsConnected);
+            }
+
+            filename.Clear();
+
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine($"ERROR: {e.Message}");
+        }
+
+        pipeServer.Close();
+    }
+}
